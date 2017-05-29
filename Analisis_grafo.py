98b5102f766055f170582adf56f240183eed06b2
EdurneMShadow <edurnecp22@gmail.com>
2017-05-29 17:39:29 +0200

    Se ha implementado toda la parte de análisis de los grafos. Al final para el grafo de followers me he quedado con los clusters que tenían más de 100 nodos y para el grafo de amigos con los que tenían entre 20 y 100, ya que eran demasiado grandes. El proceso es: se sacan los libros asociados a cada uno de los nodos y a partir de ahí se trabaja solo con los libros. Se hace la comparación de libros en una comunidad y entre varias, sacando la media de libros en cada caso. Posteriormente se calcula la media de la media y la desviación típica de las medias.

--- Analisis_grafo.py
+++ Analisis_grafo.py
@@ -54,32 +54,84 @@ def obtener_nodos_comunidades(comunidades):
         nodos_clusters.append(lista_nodos)
     return nodos_clusters
 
-'''Elimina los clusters que solo contenga un nodo'''
-def eliminar_clusters_pequenos(clusters):
-    lista_nueva_clusters = [] 
-    
+'''Elimina los clusters que solo contenga un nodo.'''
+def seleccionar_clusters(clusters, amigos):
+    lista_nueva_clusters = []     
     for cluster in clusters:
-         if len(cluster) >1:
-             lista_nueva_clusters.append(cluster)
+        if amigos:
+            if len(cluster) >20 and len(cluster)<100:
+                lista_nueva_clusters.append(cluster)
+        else:
+            if len(cluster) >100:
+                lista_nueva_clusters.append(cluster)
     return lista_nueva_clusters
 
-def eliminar_perfiles_privados(clusters,client):
-    for i in clusters:
-        for j in i:
-            try:
-                 client.get_books_user(j)
-            except Exception:
-                print(j)
-                i.remove(j)
-    return clusters
+'''Obtiene una lista de listas con los libros de los usuarios de un cluster.'''
+def get_libros(cluster, client, nombre):
+    libros = []
+    eliminados = 0
+    for i in range(len(cluster)):
+        try:
+            books = set(client.get_books_user(cluster[i]))
+            libros.append(books)
+        except Exception:
+            eliminados+=1
+    print('Nodos eliminados: ' + str(eliminados))
+    with open(nombre , 'wb') as handle:
+        pickle.dump(libros, handle, protocol= pickle.HIGHEST_PROTOCOL)
+    return libros
 
-def eliminar_clusters_vacios(clusters):
-    copia_clusters = clusters.copy()
-    for i in clusters:
-        if len(i) == 0:
-            copia_clusters.remove(i)
-    return copia_clusters
+'''Ejecuta el método anterior para un conjunto de clusters.'''
+def get_libros_comunidades(clusters, client):   
+    for i in range(len(clusters)):
+        print ('i: ' + str(i) + ' / ' + str(len(clusters)))
+        get_libros(clusters[i], client, 'lista' + str(i) + '.txt') 
+    
+'''Devuelve la media de libros en común entre dos comunidades distintas'''    
+def compare_books_comunidades_distintas(libros01, libros02):
+    cont = 0  
+    comunes = 0
+    for i in range(len(libros01)):
+        print('i: ' + str(i))
+        books = set(libros01[i])
+        for j in range(len(libros02)):
+            print('j: ' + str(j))
+            books_2 = set(libros02[j])
+            if len(books) == 0 or len(books_2) == 0:
+                print('Nada en común')
+            else:
+                comunes += len(books & books_2)
+                cont+=1
+    media = comunes / cont
+    return media 
 
+'''Devuelve la media de libros en común entre los usuarios de una misma comunidad''' 
+def compare_books_misma_comunidad(libros):
+    comunes = 0
+    cont = 0
+    for i in range(len(libros)):
+        for j in range(i+1,len(libros)):
+            print('i: ' + str(i) + ' , j: ' + str(j) + ' / ' + str(len(libros)))
+            books = set(libros[i])
+            books_2 = set(libros[j])
+            if len(books) == 0 or len(books_2) == 0:
+                print('Nada en común')
+            else:
+                comunes += len(books & books_2)**2
+                print('Sumando')
+                cont+=1
+    media = comunes / cont
+    return media
+
+'''Calcula la desviación típica para una lista de medias.'''
+def calculo_desviacion(medias_comunidades_iguales, media):
+    suma = 0
+    for i in range(len(medias_comunidades_iguales)):
+        suma+=(i-media)**2
+    desviacion = np.sqrt(suma / len(medias_comunidades_iguales))
+    return desviacion
+
+'''Devuelve un dataframe con datos para cada combinaciond de usuarios. Tarda muchísimo en ejecutar'''
 def compare_books(comunidades,client):
     df = pd.DataFrame(columns=['Usuario_01','Usuario_02','N_libros_comunes','% de U01','% de U02','Misma_comunidad'])
     cont = 0
@@ -128,46 +180,9 @@ def compare_books(comunidades,client):
                     cont+=1
     return df
   
-def compare_books_comunidades_distintas(comunidades, client):
-    df = pd.DataFrame(columns=['Usuario_01','Usuario_02','N_libros_comunes','% de U01','% de U02','Misma_comunidad'])
-    cont = 0
-    for i in range(len(comunidades)):
-        for j in comunidades:
-            if i > j:
-                for ii in i:
-                    user = ii
-                    books = set(client.get_books_user(user))
-                    for jj in j:
-                        fila = []
-                        user_2 = jj
-                        books_2 = set(client.get_books_user(user))
-                        if len(books) == 0 or len(books_2) == 0:
-                            fila.append(0)
-                            fila.append(0)
-                            fila.append(0)
-                            fila.append('no')
-                        else:
-                            comunes = books & books_2
-                            fila.append(len(comunes))
-                            fila.append(len(comunes)/len(books)*100)
-                            fila.append(len(comunes)/len(books_2)*100)
-                            fila.append('no')  
-                        df.loc[cont] = fila
-                        cont+=1
-                        
-    return df
-
-def compare_books_total(comunidades, client):
-    frames = []
-    for i in comunidades:
-        frames.append(compare_books_misma_comunidad(i,client))
-    df = pd.concat(frames)
-
-
 #Detección de comunidades
 comunidades_amigos = Graph.as_undirected(g).community_multilevel(weights=None, return_levels=False)
 comunidades_followers = f.comunidades_followers = f.community_infomap(edge_weights=None, vertex_weights=None, trials=10)
-
 
 #Guarda las variables en un fichero
 nodos_amigos = obtener_nodos_comunidades(comunidades_amigos)
@@ -185,16 +200,87 @@ with open('nodos_amigos.txt', 'rb') as handle :
 with open('nodos_followers.txt', 'rb') as handle :
     nodos_followers = pickle.load(handle)
 
+#Seleccionar los clusters con los que se va a trabajar
+clusters_amigos = seleccionar_clusters(nodos_amigos, True)
+clusters_followers = seleccionar_clusters(nodos_followers, False)
 
-clusters_amigos = eliminar_clusters_pequenos(nodos_amigos)
-clusters_followers = eliminar_clusters_pequenos(nodos_followers)
+#Sacar los libros de los clusters seleccionados           
+get_libros_comunidades(clusters_amigos, client)
 
+for i in range(25):
+    nombre = 'libros'+str(i)
+    txt = nombre + '.txt'
+    with open(txt, 'rb') as handle :
+        nombre = pickle.load(handle)
 
+lista_libros_amigos = (lista0 + lista1 +lista2 + lista3+ lista4+lista5+lista6+
+                      lista7+liesta8+lista9+lista10+lista11+lista12+lista13+
+                      lista14+lista15+lista16+lista17+lista18+lista19+lista20+
+                      lista21+lista22+lista23+lista24)
 
-            
+#Cálculo de la media de libros de cada una de las comunidades
+medias_iguales_amigos = []
+for i in lista_libros_amigos:
+    medias_iguales_amigos.append(compare_books_misma_comunidad(i))
 
+medias_distintas_amigos = []
+for i in range(len(lista_libros_amigos)):
+    for j in range(i+1, len(lista_libros_amigos)):
+        medias_distintas_amigos.append(compare_books_comunidades_distintas(lista_libros_amigos[i], lista_libros_amigos[j]))
 
+suma = 0
+for i in medias_iguales_amigos:
+    suma+=i
+media_iguales_amigos_total = suma / len(medias_iguales_amigos)
 
+suma = 0
+for i in medias_distintas_amigos:
+    suma+=i
+media_distintas_amigos_total = suma / len(medias_distintas_amigos)
 
+desviacion_iguales_amigos = calculo_desviacion(medias_iguales_amigos, media_iguales_amigos_total)
+desviacion_distintas_amigos = calculo_desviacion(medias_distintas_amigos, media_distintas_amigos_total)
 
+print('Grafo de amigos')
+print('Misma comunidad: Media:' + str(media_iguales_amigos_total) + ' ,Desviacion_típica:' + str(desviacion_iguales_amigos))
+print('Distintas comunidades: Media:' + str(media_distintas_amigos_total) + ' ,Desviacion_típica:' + str(desviacion_distintas_amigos))
+#Análisis del grafo de followers
+get_libros_comunidades(clusters_followers, client)
 
+for i in range(25):
+    nombre = 'libros'+str(i)
+    txt = nombre + '.txt'
+    with open(txt, 'rb') as handle :
+        nombre = pickle.load(handle)
+
+lista_libros_followers = (lista0 + lista1 +lista2 + lista3+ lista4+lista5+lista6+
+                      lista7+liesta8+lista9+lista10+lista11+lista12+lista13+
+                      lista14+lista15+lista16+lista17+lista18+lista19+lista20+
+                      lista21+lista22+lista23+lista24)
+
+#Cálculo de la media de libros de cada una de las comunidades
+medias_iguales_followers = []
+for i in lista_libros_followers:
+    medias_iguales_followers.append(compare_books_misma_comunidad(i))
+
+medias_distintas_followers = []
+for i in range(len(lista_libros_followers)):
+    for j in range(i+1, len(lista_libros_followers)):
+        medias_distintas_followers.append(compare_books_comunidades_distintas(lista_libros_followers[i], lista_libros_followers[j]))
+
+suma = 0
+for i in medias_iguales_followers:
+    suma+=i
+media_iguales_followers_total = suma / len(medias_iguales_followers)
+
+suma = 0
+for i in medias_distintas_followers:
+    suma+=i
+media_distintas_followers_total = suma / len(medias_distintas_followers)
+
+desviacion_iguales_followers = calculo_desviacion(medias_iguales_followers, media_iguales_followers_total)
+desviacion_distintas_followers = calculo_desviacion(medias_distintas_followers, media_distintas_followers_total)
+
+print('Grafo de followers')
+print('Misma comunidad: Media:' + str(media_iguales_followers_total) + ' ,Desviacion_típica:' + str(desviacion_iguales_followers))
+print('Distintas comunidades: Media:' + str(media_distintas_followers_total) + ' ,Desviacion_típica:' + str(desviacion_distintas_followers))
